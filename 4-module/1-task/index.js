71a536bfcfabef9394e638d961a8ff77c6cd9b1f
Dmitriy Tararoshchenko <31081938+6epc@users.noreply.github.com>
2020-02-13 02:08:31 +0300

    4-module/1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,16 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let ul = document.createElement('ul');
+
+  for (let friend of friends) {
+
+    let li = document.createElement('li');
+
+    li.innerHTML = friend.firstName + ' ' + friend.lastName;
+
+    ul.append(li);
+  }
+
+  return ul;
 }
