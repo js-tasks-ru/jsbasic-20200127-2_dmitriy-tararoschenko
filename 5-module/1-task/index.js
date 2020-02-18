a5f9ae98d014b1416fe5949f1dd3e3ed7ee1b729
Dmitriy Tararoshchenko <31081938+6epc@users.noreply.github.com>
2020-02-18 17:37:21 +0300

    5-module/1-task

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,34 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+  let rows = table.querySelectorAll('tbody tr');
+
+  for (let row of rows) {
+
+    let status = row.cells[3].dataset.available;
+
+    if (status === 'true') {
+      row.classList.add('available');
+    } else if (status === 'false') {
+      row.classList.add('unavailable');
+    } else {
+      row.setAttribute('hidden', 'true');
+    }
+
+    let genderValue = row.cells[2].innerHTML;
+
+    if (genderValue == 'm') {
+      row.classList.add('male');
+    } else if (genderValue == 'f') {
+      row.classList.add('female');
+    }
+
+    let ageValue = row.cells[1].innerHTML;
+
+    if (ageValue < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+
+  }
 }
