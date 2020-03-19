c391f468ac759b7202dc767303abbe8505d291a2
Dmitriy Tararoshchenko <31081938+6epc@users.noreply.github.com>
2020-03-19 20:07:36 +0300

    5-module/2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -13,17 +13,58 @@
  *
  * @constructor
  */
-function SortableTable(items) {
-  /**
-   * @property {Element} - обязательное свойство, которое ссылается на элемент <table>
-   */
-  this.el = document.createElement('table');
+class SortableTable {
 
-  /**
-   * Метод выполняет сортировку таблицы
-   * @param {number} column - номер колонки, по которой
-   * нужно выполнить сортировку (отсчет начинается от 0)
-   * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
-   */
-  this.sort = (column, desc = false) => {};
+  constructor(rows) {
+    this.el = document.createElement('table');
+    this.el.innerHTML = `<thead>
+                                <tr>
+                                  <td>Name</td>
+                                  <td>Age</td>
+                                  <td>Salary</td>
+                                  <td>City</td>
+                                </tr>
+                              </thead>
+                              <tbody></tbody>`;
+
+    let tbody = this.el.querySelector('tbody');
+
+    for (let row of rows) {
+      let tr = document.createElement('tr');
+
+      for (const key in row) {
+        tr.innerHTML += `<td>${row[key]}</td>`;
+      }
+      tbody.append(tr);
+    }
+  }
+
+  sort(column, desc = false) {
+    let tbody = this.el.querySelector('tbody');
+    let rowsForSort = Array.from(tbody.rows);
+
+    if (desc) {
+      rowsForSort.sort((a, b) => {
+        a = a.cells[column].innerHTML;
+        b = b.cells[column].innerHTML;
+        if (b > a) return 1;
+        if (b == a) return 0;
+        if (b < a) return -1;
+      });
+    } else {
+      rowsForSort.sort((a, b) => {
+        a = a.cells[column].innerHTML;
+        b = b.cells[column].innerHTML;
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+      });
+    }
+
+    for (let row of rowsForSort) {
+      tbody.append(row);
+    }
+
+  };
+
 }
